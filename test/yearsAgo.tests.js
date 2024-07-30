a8e2cb9b1e3bdd7651d31eb77c58ff6381e9691f
Billie-Matemba <billiematemba133@gmail.com>
2024-07-30 11:53:33 +0200

    final commit

--- test/yearsAgo.tests.js
+++ test/yearsAgo.tests.js
@@ -1,13 +1,13 @@
 describe('yearsAgo Test' , function(){
     it('should return "48" when called with 1976' , function(){
-        assert.equal((new Date().getFullYear() - 1976), 48) 
+        assert.equal((yearsAgo(1976)), 48) 
     });
 
     it('should return "24" when called with 2000' , function(){
-        assert.equal((new Date().getFullYear() - 2000), 24);
+        assert.equal((yearsAgo(2000)), 24);
     });
 
     it('should return "20" when called with 2004' , function(){
-        assert.equal((new Date().getFullYear() - 2004), 20);
+        assert.equal((yearsAgo(2004)), 20);
     });
 });
